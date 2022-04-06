d4c8b50e91b23e9c619ec3b8539075ebdaf5552b
Vinod Pasalkar <vinodpasalkar@gmail.com>
2022-04-06 23:16:09 +0100

    Initial framework skeleton

---
+++ src/test/java/test/java/news/utils/PropertiesReader.java
@@ -0,0 +1,31 @@
+package test.java.news.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader {
+    private final static String pathToConfigFile = "config.properties";
+    private static Properties properties = null;
+    private static String filePointer = "";
+
+    private static void setProperties(String fileName) throws
+            IOException {
+        properties = new Properties();
+        if (pathToConfigFile.contains(fileName)) {
+            properties.load(new FileInputStream(pathToConfigFile));
+            filePointer = pathToConfigFile;
+        }
+    }
+
+    public static String getProperty(String fileName, String key) {
+        if (!(properties != null && filePointer.contains(fileName))) {
+            try {
+                setProperties(fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return properties.getProperty(key);
+    }
+}
